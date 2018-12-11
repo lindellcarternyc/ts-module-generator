b280e72e4b739e3c5587444d130b39a1245e3f5e
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-11 17:17:24 -0500

    fix linting errors

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import program from 'commander'
 
 import { generateFunction } from './main'
 
-program
-  .version('1.0.0')
-  .description('Generators for Typescript built on node!')
+program.version('1.0.0').description('Generators for Typescript built on node!')
 
 program
   .command('createFunction [rootDir]')
   .description('Create a new function')
-  .action(async (rootDir) => {
+  .action(async rootDir => {
     try {
       await generateFunction(rootDir || 'src')
-    } catch ( err ) {
+    } catch (err) {
       throw err
     }
   })
