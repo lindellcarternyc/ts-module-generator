9584e440af0ea2372b2167d69603770f2f2c8d9e
Lindell Carter <lindellcarternyc@gmail.com>
2018-12-11 11:50:35 -0500

    update writeFile tests

--- src/writeFile.test.ts
+++ src/writeFile.test.ts
@@ -1,14 +1,20 @@
 import fs from 'fs'
 import path from 'path'
+import rimraf from 'rimraf'
 import writeFile from './writeFile'
 
 describe('writeFile', () => {
   it('should write a file to the designated path', async () => {
-    const filepath = path.resolve(process.cwd(), 'testDir', 'testDir1', 'testFile.ts')
+    const dirPath = path.resolve(process.cwd(), 'testDir', 'testDir1')
+    const filename = 'testFile.ts'
+    const filepath = path.resolve(dirPath, filename)
     try {
       await writeFile(filepath, 'hello world')
-      const files = fs.readdirSync(path.resolve(process.cwd(), 'testDir', 'testDir1'))
+      const files = fs.readdirSync(dirPath)
       expect(files.includes('testFile.ts'))
+      rimraf(filepath, err => {
+        if ( err ) { throw err }
+      })
     } catch ( err ) {
       throw err
     }
